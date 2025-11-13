c348351aca27cec06596e92799422274b0d8b71b
Cristian Acero <cristeel04@gmail.com>
2025-11-13 00:19:32 -0300

    Add files via upload

---
+++ LPV/LPV/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include  # 👈 nota el 'include'
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('miapp.urls')),  # 👈 esto enlaza las URLs de tu app
+    
+]
